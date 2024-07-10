401d426f76412c59408e791aa5d7a8fabe28dc2b
Pedro Henrique Cardoso dos Santos <165037293+pedrohcsantos@users.noreply.github.com>
2024-07-10 11:47:37 -0300

    Add files via upload

---
+++ aula10_07_2024/alomundo.py
@@ -0,0 +1,8 @@
+print("Alô mundo dos Algoritmos!")
+#print("Eu amo o Satoru Gojo \n" * 10)
+#Essa linha o python não vai ler. É só para humanos
+E = 7 #Minha primeira variável
+print(type(E))
+print(E)
+nome = input("Digite seu nome: ")
+print("VOLTA, " + nome + "!")
