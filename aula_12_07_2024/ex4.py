56c46ff57b51e7714fa819453023af6d7dfcfe3e
Pedro Henrique Cardoso dos Santos <165037293+pedrohcsantos@users.noreply.github.com>
2024-07-12 10:15:12 -0300

    Update ex4.py

--- aula_12_07_2024/ex4.py
+++ aula_12_07_2024/ex4.py
@@ -1,6 +1,13 @@
-#Faça um programa que converta graus celcius para Fahrenheit
+#Faça um programa que converta graus celcius para Fahrenheitwhile != "C" or "F"
 
-celcius = float(input("Escreva o valor da temperatura em celcius: "))
-tc = celcius/5
-tf = (tc*9)+32
-print(f"O valor de {celcius} graus celcius em graus Fahrenheit {tf} é de ")
+temp = input("Escolha a temperatura que vai ser convertida: Celsius(C) ou Fahrenheit(F): ")
+while temp != "C" and temp != "F":
+        temp = input("Escolha a temperatura certa: Celsius(C) ou Fahrenheit(F): ")
+if temp == "C":
+        tc = float(input("Escreva o valor da temperatura em celcius: "))
+        tf = ((tc/5)*9)+32
+        print(f"O valor de {tc:.2f} graus celcius em graus Fahrenheit {tf:.2f} é de ")
+elif temp == "F":
+        tf = float(input("Escreva o valor da temperatura em Fahrenheit: "))
+        tc = ((tf -32)/9)*5
+        print(f"O valor de {tf:.2f} graus Fahrenheit em graus Celcius {tc:.2f} é de ")
