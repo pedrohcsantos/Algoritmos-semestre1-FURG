b88a0b93406dea9e882b5e7c8fe5a8b13c178cc9
Pedro Henrique Cardoso dos Santos <165037293+pedrohcsantos@users.noreply.github.com>
2024-07-12 09:46:15 -0300

    Update ex4.py

--- aula_12_07_2024/ex4.py
+++ aula_12_07_2024/ex4.py
@@ -1,6 +1,6 @@
-#Faça um programa que converta graus celcius para Farenheit
+#Faça um programa que converta graus celcius para Fahrenheit
 
 celcius = float(input("Escreva o valor da temperatura em celcius: "))
 tc = celcius/5
 tf = (tc*9)+32
-print(f"O valor de {celcius} graus celcius em graus {tf} é de ")
+print(f"O valor de {celcius} graus celcius em graus Fahrenheit {tf} é de ")
