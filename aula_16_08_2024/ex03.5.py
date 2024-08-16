0dfe719280c73464e43b087cddf89a1ca4d323cb
Pedro Henrique Cardoso dos Santos <165037293+pedrohcsantos@users.noreply.github.com>
2024-08-16 10:34:11 -0300

    Update ex03.5.py

--- aula_16_08_2024/ex03.5.py
+++ aula_16_08_2024/ex03.5.py
@@ -3,19 +3,20 @@
 x = str(input("Informe um nome: "))
 cont = 0
 palavra = ' '
+espelho = ' '
+cont_palavras = 0
 
 
 while cont < len(x):
+    palavra = x[cont] + palavra
+    cont += 1
     if x[cont] == ' ':
-        palavra = x[cont] + palavra
-        cont_palavra = len(palavra)
-        cont += 1
-        while x[cont_palavra-1] != ' ':
+        while x[cont_palavra] != x[cont]:
             cont_palavra = cont_palavra - 1
             espelho = palavra + palavra[cont_palavra -1]
             espelho = espelho + ' '
+            cont_palavras += 1
     palavra = x[cont] + palavra
     cont_palavra = len(palavra)
     cont += 1
 print(espelho)
-        
