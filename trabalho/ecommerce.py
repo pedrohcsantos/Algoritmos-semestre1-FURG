0ed8bbf3eff426d1652fc02b786e6f34ac1e9dfd
Pedro Henrique Cardoso dos Santos <165037293+pedrohcsantos@users.noreply.github.com>
2025-01-07 22:35:56 -0300

    Trabalho de e-ccomerce

---
+++ trabalho/ecommerce.py
@@ -0,0 +1,103 @@
+import graphics as gp
+import csv
+
+CSV_FILE = "estoque_ecommerce.csv"
+
+def cria_botao(x1,y1,x2,y2):
+    novo_botao = gp.Rectangle(gp.Point(x1,y1), gp.Point(x2,y2))
+    novo_botao.setFill('white')
+    novo_botao.draw(win)
+    botoes.append(novo_botao)
+    return novo_botao
+
+def foi_no_botao(botao,onde_cliquei):
+    btX = estoque.getP1().getX()
+    btY = estoque.getP1().getY()
+    bt2X = estoque.getP2().getX()
+    bt2Y = estoque.getP2().getY()
+    print(f'({btX},{btY})({bt2X},{bt2Y}) --- ({onde_cliquei.getX()},{onde_cliquei.getX()})({onde_cliquei.getY()},{onde_cliquei.getY()})')
+    if onde_cliquei.getX() > btX and onde_cliquei.getX() < bt2X and onde_cliquei.getY() < btY and onde_cliquei.getY() > bt2Y:
+        return True
+    return False
+
+def estoquecsv():
+    with open('estoque.csv','r') as arq:
+        linhas = arq.readlines()
+    linhas_sem_cabecalho = linhas[1:]
+    estoque = []
+    for linha in linhas_sem_cabecalho:
+        dados = linha.strip().split(',')
+        estoque.append(dados)
+    return estoque
+
+def verificar_estoque():
+    cont = 1
+    title = gp.Circle(gp.Point(300,20),15)
+    ver_estoque = estoquecsv()
+    win = gp.GraphWin(  "Estoque"  , 600, 450)
+    titulo = gp.Text(title.getCenter(), 'Estoque Atual')
+    titulo.setSize(18)
+    titulo.setStyle('bold')
+    titulo.draw(win)
+    retangulo_id = gp.Rectangle(gp.Point(30, 100), gp.Point(100, 70))
+    retangulo_id.setFill('lightblue')
+    retangulo_id.setOutline('black')
+    retangulo_id.draw(win)
+    retangulo_id_text = gp.Text(retangulo_id.getCenter(), 'ID')
+    retangulo_id_text.draw(win)
+    retangulo_nome = gp.Rectangle(gp.Point(101, 100), gp.Point(240, 70))
+    retangulo_nome.setFill('lightblue')
+    retangulo_nome.setOutline('black')
+    retangulo_nome.draw(win)
+    retangulo_nome_text = gp.Text(retangulo_nome.getCenter(), 'Nome')
+    retangulo_nome_text.draw(win)
+    retangulo_valor = gp.Rectangle(gp.Point(241, 100), gp.Point(320, 70))
+    retangulo_valor.setFill('lightblue')
+    retangulo_valor.setOutline('black')
+    retangulo_valor.draw(win)
+    retangulo_valor_text = gp.Text(retangulo_valor.getCenter(), 'Valor')
+    retangulo_valor_text.draw(win)
+    retangulo_estoque = gp.Rectangle(gp.Point(321, 100), gp.Point(400, 70))
+    retangulo_estoque.setFill('lightblue')
+    retangulo_estoque.setOutline('black')
+    retangulo_estoque.draw(win)
+    retangulo_estoque_text = gp.Text(retangulo_estoque.getCenter(), 'Estoque')
+    retangulo_estoque_text.draw(win)
+    retangulo_quantidade = gp.Rectangle(gp.Point(401, 100), gp.Point(570, 70))
+    retangulo_quantidade.setFill('lightblue')
+    retangulo_quantidade.setOutline('black')
+    retangulo_quantidade.draw(win)
+    retangulo_quantidade_text = gp.Text(retangulo_quantidade.getCenter(), 'Quantidade Vendida')
+    retangulo_quantidade_text.draw(win)
+
+                
+
+
+
+win = gp.GraphWin(  "Loja"  , 500, 450)
+click = gp.Point(0,0)
+botoes = []
+
+
+estoque = cria_botao(20,300,230,250)
+texto = gp.Text(estoque.getCenter(), 'Verificar Estoque')
+texto.draw(win)
+
+peca = cria_botao(270,300,480,250)
+texto = gp.Text(peca.getCenter(), 'Cadastras novas peças')
+texto.draw(win)
+
+venda = cria_botao(20,400,230,350)
+texto = gp.Text(venda.getCenter(), 'Registrar Venda')
+texto.draw(win)
+
+peca = cria_botao(270,300,480,250)
+texto = gp.Text(peca.getCenter(), 'Cadastras novas peças')
+texto.draw(win)
+
+
+
+while True:
+    onde_cliquei = win.getMouse()
+    if foi_no_botao(estoque,onde_cliquei):
+        verificar_estoque()
